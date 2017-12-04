8c757be7a17357ec003ce3f86d49c4e3bfbc766b
Jelle Stege <jstege1206@gmail.com>
2017-12-04 17:08:55 +0100

    Fixed bug in 2017/4

--- aoc-2017/src/main/kotlin/nl/jstege/adventofcode/aoc2017/days/Day04.kt
+++ aoc-2017/src/main/kotlin/nl/jstege/adventofcode/aoc2017/days/Day04.kt
@@ -9,7 +9,7 @@ import nl.jstege.adventofcode.aoccommon.days.Day
 class Day04 : Day() {
     override fun first(input: Sequence<String>): Any = input.parse { it }
 
-    override fun second(input: Sequence<String>): Any = input.parse { it.toSet() }
+    override fun second(input: Sequence<String>): Any = input.parse { it.toList().sorted() }
 
     private inline fun <E> Sequence<String>.parse(mapFunction: (String) -> E): Int =
             this.map { it.split(' ') }.count { it.size == it.map(mapFunction).toSet().size }
