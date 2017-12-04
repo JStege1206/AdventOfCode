6c4973128fb23584854151f6236db1d28b007917
Jelle Stege <jstege1206@gmail.com>
2017-12-04 14:56:33 +0100

    Updated 2017/4

--- aoc-2017/src/main/kotlin/nl/jstege/adventofcode/aoc2017/days/Day04.kt
+++ aoc-2017/src/main/kotlin/nl/jstege/adventofcode/aoc2017/days/Day04.kt
@@ -7,16 +7,10 @@ import nl.jstege.adventofcode.aoccommon.days.Day
  * @author Jelle Stege
  */
 class Day04 : Day() {
-    override fun first(input: Sequence<String>): Any {
-        return input.parse { this }
-    }
+    override fun first(input: Sequence<String>): Any = input.parse { it }
 
-    override fun second(input: Sequence<String>): Any {
-        return input.parse { this.map { it.toSet() } }
-    }
+    override fun second(input: Sequence<String>): Any = input.parse { it.toSet() }
 
-    private inline fun <E> Sequence<String>.parse(mapper: (List<String>.() -> List<E>)): Int =
-            this
-                    .map { it.split(' ') }
-                    .count { it.size == it.mapper().toSet().size }
+    private inline fun <E> Sequence<String>.parse(mapFunction: (String) -> E): Int =
+            this.map { it.split(' ') }.count { it.size == it.map(mapFunction).toSet().size }
 }
