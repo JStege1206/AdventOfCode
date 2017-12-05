5c10c0daeb5c5f81a933e2b185ac5de23e343289
Jelle Stege <jstege1206@gmail.com>
2017-12-05 14:24:53 +0100

    Massive performance update for 2017/5.
    Retrieving array value only once cuts execution time by 2/3

--- aoc-2017/src/main/kotlin/nl/jstege/adventofcode/aoc2017/days/Day05.kt
+++ aoc-2017/src/main/kotlin/nl/jstege/adventofcode/aoc2017/days/Day05.kt
@@ -8,20 +8,28 @@ import nl.jstege.adventofcode.aoccommon.days.Day
  */
 class Day05 : Day() {
     override fun first(input: Sequence<String>): Any {
-        return input.map(String::toInt).toList().toIntArray().walk { 1 }
+        return input
+                .map(String::toInt)
+                .toList()
+                .toIntArray()
+                .walk { 1 }
     }
 
     override fun second(input: Sequence<String>): Any {
-        return input.map(String::toInt).toList().toIntArray().walk { if (it >= 3) -1 else 1 }
+        return input
+                .map(String::toInt)
+                .toList()
+                .toIntArray()
+                .walk { if (it >= 3) -1 else 1 }
     }
 
     private fun IntArray.walk(incrementalStep: (Int) -> Int): Int {
         tailrec fun walk(i: Int, accumulator: Int = 0): Int {
             if (i >= this.size) return accumulator
-            val t = i + this[i]
-            this[i] += incrementalStep(this[i])
-            return walk(t, accumulator + 1)
+            val t = this[i]
+            this[i] += incrementalStep(t)
+            return walk(i + t, accumulator + 1)
         }
-        return walk(0, 0)
+        return walk(0)
     }
 }
