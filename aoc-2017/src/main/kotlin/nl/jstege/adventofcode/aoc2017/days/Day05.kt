26a58c9f54469b9883a1ab75cf25a87c46601f28
Jelle Stege <jstege1206@gmail.com>
2017-12-05 08:25:47 +0100

    Added 2017/5, tail recursion to the rescue!

--- aoc-2017/src/main/kotlin/nl/jstege/adventofcode/aoc2017/days/Day05.kt
+++ aoc-2017/src/main/kotlin/nl/jstege/adventofcode/aoc2017/days/Day05.kt
@@ -7,11 +7,21 @@ import nl.jstege.adventofcode.aoccommon.days.Day
  * @author Jelle Stege
  */
 class Day05 : Day() {
-    override fun first(input: Sequence<String>): Any {//TODO: implement
-        return ""
+    override fun first(input: Sequence<String>): Any {
+        return input.map { it.toInt() }.toList().toIntArray().walk { 1 }
     }
 
-    override fun second(input: Sequence<String>): Any {//TODO: implement
-        return ""
+    override fun second(input: Sequence<String>): Any {
+        return input.map { it.toInt() }.toList().toIntArray().walk { if (it >= 3) -1 else 1}
+    }
+
+    private fun IntArray.walk(incrementalStep: (Int) -> Int): Int {
+        tailrec fun IntArray.walk(i: Int, accumulator: Int = 0): Int {
+            if (i >= this.size) return accumulator
+            val t = i + this[i]
+            this[i] += incrementalStep(this[i])
+            return this.walk(t, accumulator + 1)
+        }
+        return this.walk(0, 0)
     }
 }
