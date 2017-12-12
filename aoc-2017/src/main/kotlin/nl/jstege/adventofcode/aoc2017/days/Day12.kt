11d51b29f98de40e20625d6be67fac38f104ab38
Jelle Stege <jstege1206@gmail.com>
2017-12-12 10:39:56 +0100

    Added 2017/12. Not happy with my solution yet.

--- aoc-2017/src/main/kotlin/nl/jstege/adventofcode/aoc2017/days/Day12.kt
+++ aoc-2017/src/main/kotlin/nl/jstege/adventofcode/aoc2017/days/Day12.kt
@@ -7,11 +7,34 @@ import nl.jstege.adventofcode.aoccommon.days.Day
  * @author Jelle Stege
  */
 class Day12 : Day() {
-    override fun first(input: Sequence<String>): Any {//TODO: implement
-        return ""
+    override fun first(input: Sequence<String>): Any {
+        return countRoutes(0, input.buildConnections(), mutableSetOf())
     }
 
-    override fun second(input: Sequence<String>): Any {//TODO: implement
-        return ""
+    override fun second(input: Sequence<String>): Any {
+        val visited = mutableSetOf<Int>()
+        val pipes = input.buildConnections()
+
+        var groups = 0
+        while (true) {
+            val f = pipes.keys.find { it !in visited } ?: break
+            countRoutes(f, pipes, visited)
+            groups++
+        }
+        return groups
     }
+
+    private fun countRoutes(pipe: Int, pipes: Map<Int, Set<Int>>, visited: MutableSet<Int>): Int {
+        if (pipe in visited) return 0
+        visited += pipe
+        return 1 + pipes[pipe]!!
+                .asSequence()
+                .filter { it !in visited }
+                .sumBy { countRoutes(it, pipes, visited) }
+    }
+
+    private fun Sequence<String>.buildConnections(): Map<Int, Set<Int>> = this
+            .map { it.split(" <-> ") }
+            .map { it.first().toInt() to it[1].split(", ").map { it.toInt() }.toSet() }
+            .toMap()
 }
