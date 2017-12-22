178048548404c2e52e2ba3cdb5b9ccb448bb3023
Jelle Stege <jstege1206@gmail.com>
2017-12-22 19:24:05 +0100

    Updated 2017/22. Make use of an actual grid instead of a map of points.

--- aoc-2017/src/main/kotlin/nl/jstege/adventofcode/aoc2017/days/Day22.kt
+++ aoc-2017/src/main/kotlin/nl/jstege/adventofcode/aoc2017/days/Day22.kt
@@ -11,8 +11,8 @@ import kotlin.reflect.KProperty1
  */
 class Day22 : Day() {
     private companion object Configuration {
-        const val CLEAN_NODE = '.'
-        const val INFECTED_NODE = '#'
+        const val CLEAN_CHAR = '.'
+        const val INFECTED_CHAR = '#'
         const val FIRST_ITERATIONS = 10000
         const val SECOND_ITERATIONS = 10000000
 
@@ -45,28 +45,32 @@ class Day22 : Day() {
     }
 
     private fun Sequence<String>.parse() = this
-            .foldIndexed(mapOf<Point, Status>()) { y, nodes, line ->
-                nodes + line.mapIndexed { x, c -> Point.of(x, y) to Status.of(c) }.toMap()
+            .foldIndexed(mutableMapOf<Point, Node>()) { y, nodes, line ->
+                nodes += line
+                        .mapIndexed { x, c -> Point.of(x, y) to c }
+                        .map { (p, c) -> p to Node(p, nodes, Status.of(c)) }
+                        .toMap()
+                nodes
             }
             .toMutableMap()
 
-    private fun MutableMap<Point, Status>.work(bursts: Int, stateMod: (Status) -> Status): Int {
-        var currentLocation = Point.of(
+    private fun MutableMap<Point, Node>.work(bursts: Int, stateMod: (Status) -> Status): Int {
+        val currentLocation = Point.of(
                 this.keys.map { it.x }.max()!! / 2,
                 this.keys.map { it.y }.max()!! / 2)
+        var node = this.getOrPut(currentLocation) { Node(currentLocation, this) }
         var direction = INITIAL_DIRECTION
-        
+
         return (0 until bursts)
                 .count {
-                    val oldState = this[currentLocation] ?: Status.CLEAN
-                    val newState = stateMod(oldState)
-                    this[currentLocation] = newState
-                    direction = oldState.directionMod.get(direction)
-                    currentLocation = currentLocation.moveDirection(direction)
+                    direction = node.status.directionMod.get(direction)
+                    val newState = stateMod(node.status)
+                    node.status = newState
+                    node = node.move(direction)
                     newState == Status.INFECTED
                 }
     }
-
+    
     private enum class Status(val directionMod: KProperty1<Direction, Direction>) {
         INFECTED(Direction::right),
         CLEAN(Direction::left),
@@ -75,10 +79,39 @@ class Day22 : Day() {
 
         companion object {
             fun of(c: Char) = when (c) {
-                CLEAN_NODE -> CLEAN
-                INFECTED_NODE -> INFECTED
+                CLEAN_CHAR -> CLEAN
+                INFECTED_CHAR -> INFECTED
                 else -> throw IllegalArgumentException()
             }
+        }
+    }
+
+    private class Node(
+            val coordinate: Point,
+            val cache: MutableMap<Point, Node>,
+            var status: Status = Status.CLEAN) {
+        val left by lazy {
+            val np = coordinate.decX()
+            cache.getOrPut(np, { Node(np, cache) })
+        }
+        val right by lazy {
+            val np = coordinate.incX()
+            cache.getOrPut(np, { Node(np, cache) })
+        }
+        val up by lazy {
+            val np = coordinate.decY()
+            cache.getOrPut(np, { Node(np, cache) })
+        }
+        val down by lazy {
+            val np = coordinate.incY()
+            cache.getOrPut(np, { Node(np, cache) })
+        }
+        
+        fun move(dir: Direction) = when(dir) {
+            Direction.NORTH -> up
+            Direction.EAST -> right
+            Direction.SOUTH -> down
+            Direction.WEST -> left
         }
     }
 }
