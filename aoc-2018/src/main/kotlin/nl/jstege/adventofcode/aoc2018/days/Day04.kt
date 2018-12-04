f2f20d63dd85ee2fb8dc27b81da5bc4e1dfb790a
Jelle Stege <jstege1206@gmail.com>
2018-12-04 18:25:44 +0100

    Updated 2018#04

--- aoc-2018/src/main/kotlin/nl/jstege/adventofcode/aoc2018/days/Day04.kt
+++ aoc-2018/src/main/kotlin/nl/jstege/adventofcode/aoc2018/days/Day04.kt
@@ -19,35 +19,38 @@ class Day04 : Day(title = "Repose Record") {
         } ?: throw IllegalStateException()
 
     override fun second(input: Sequence<String>): Any = input
-        .parse()
-        .mapValues { (_, sleepSessions) ->
+        .parse().entries
+        .mapNotNull { (g, sleepSessions) ->
             sleepSessions
                 .findMinuteMostAsleep()
-                ?.let { (minute, amount) -> minute to amount } ?: (0 to 0)
+                ?.let { (minute, amount) -> Triple(g, minute, amount) }
         }
-        .maxBy { (_, minuteAmount) -> minuteAmount.second }
-        ?.let { (g, p) -> g * p.first } ?: throw IllegalStateException()
+        .maxBy { (_, _, minuteAmount) -> minuteAmount }
+        ?.let { (g, minute) ->
+            g * minute
+        } ?: throw IllegalStateException()
 
-    private fun Sequence<String>.parse() = mutableMapOf<Int, MutableList<List<Int>>>().apply {
-        this@parse
-            .sorted()
-            .map { it.extractValues(INPUT_REGEX, 2, 3, 4) }
-            .fold(-1 to mutableListOf<List<Int>>()) { (startTime, sessions), (minute, op, id) ->
-                when (op) {
-                    "falls asleep" -> minute.toInt() to sessions
-                    "wakes up" -> -1 to sessions.apply { add(startTime.minutesTo(minute.toInt())) }
-                    else -> -1 to computeIfAbsent(id.toInt()) { mutableListOf() }
+    private fun Sequence<String>.parse(): Map<Int, List<List<Int>>> =
+        mutableMapOf<Int, MutableList<List<Int>>>().apply {
+            this@parse
+                .sorted()
+                .map { it.extractValues(INPUT_REGEX, 2, 3, 4) }
+                .fold(-1 to mutableListOf<List<Int>>()) { (start, sessions), (minute, op, id) ->
+                    when (op) {
+                        "falls asleep" -> minute.toInt() to sessions
+                        "wakes up" -> -1 to sessions.apply { add(start.minutesTo(minute.toInt())) }
+                        else -> -1 to computeIfAbsent(id.toInt()) { mutableListOf() }
+                    }
                 }
-            }
-    }
+        }.toMap()
 
-    private fun Int.minutesTo(other: Int) =
+    private fun Int.minutesTo(other: Int): List<Int> =
         if (this >= other) (this until (other + 60)).map { it % 60 }
         else (this until other).toList()
 
-    private fun List<List<Int>>.findMinuteMostAsleep() = this
+    private fun List<List<Int>>.findMinuteMostAsleep(): Map.Entry<Int, Int>? = this
         .flatten()
-        .groupingBy { it }
-        .eachCount()
+        .groupBy { it }
+        .mapValues { (_, value) -> value.size }
         .maxBy { it.value }
 }
