83856c421a15e7764c563e335d3503c0f6190321
Jelle Stege <jstege1206@gmail.com>
2018-12-12 19:48:58 +0100

    Added 2018#12

--- aoc-2018/src/main/kotlin/nl/jstege/adventofcode/aoc2018/days/Day12.kt
+++ aoc-2018/src/main/kotlin/nl/jstege/adventofcode/aoc2018/days/Day12.kt
@@ -1,13 +1,51 @@
 package nl.jstege.adventofcode.aoc2018.days
 
 import nl.jstege.adventofcode.aoccommon.days.Day
+import nl.jstege.adventofcode.aoccommon.utils.extensions.head
+import nl.jstege.adventofcode.aoccommon.utils.extensions.sumBy
 
-class Day12 : Day(title = "") {
+class Day12 : Day(title = "Subterranean Sustainability") {
+    private companion object Configuration {
+        private const val FIRST_GENERATIONS = 20L
+        private const val SECOND_GENERATIONS = 50_000_000_000L
+    }
+
     override fun first(input: Sequence<String>): Any {
-        TODO("Implement")
+        return input.parse().let { (initial, reps) ->
+            initial.solve(FIRST_GENERATIONS, reps)
+        }
     }
 
     override fun second(input: Sequence<String>): Any {
-        TODO("Implement")
+        return input.parse().let { (initial, reps) ->
+            initial.solve(SECOND_GENERATIONS, reps)
+        }
     }
+
+    private fun String.solve(generations: Long, replacements: Map<String, String>): Long {
+        tailrec fun String.solveRecursive(currentGen: Long, shift: Long): Long {
+            val (nextState, nextShift) = this.next(shift, replacements)
+            if (nextState == this || currentGen == generations) {
+                val absoluteShift = shift + (nextShift - shift) * (generations - currentGen)
+                return this
+                    .withIndex()
+                    .filter { it.value == '#' }
+                    .sumBy { it.index + absoluteShift }
+            }
+            return nextState.solveRecursive(currentGen + 1, nextShift)
+        }
+        return this.solveRecursive(0, 0)
+    }
+
+    private fun String.next(shift: Long, replacements: Map<String, String>): Pair<String, Long> =
+        "....$this...."
+            .windowed(5, 1)
+            .joinToString("") { replacements[it] ?: "." }
+            .let { state -> Pair(state.trim('.'), shift - 2 + state.indexOf('#')) }
+
+    private fun Sequence<String>.parse(): Pair<String, Map<String, String>> =
+        Pair(
+            head.removePrefix("initial state: "),
+            drop(2).map { it.split(" => ") }.associate { (f, s) -> f to s }
+        )
 }
--- aoc-common/src/main/kotlin/nl/jstege/adventofcode/aoccommon/Tuples.kt
+++ aoc-common/src/main/kotlin/nl/jstege/adventofcode/aoccommon/Tuples.kt
@@ -12,3 +12,13 @@ data class Quadruple<out A, out B, out C, out D>(
 ) {
     override fun toString() = "($first, $second, $third, $fourth)"
 }
+
+data class Quintuple<out A, out B, out C, out D, out E>(
+    val first: A,
+    val second: B,
+    val third: C,
+    val fourth: D,
+    val fifth: E
+) {
+    override fun toString() = "($first, $second, $third, $fourth, $fifth)"
+}
