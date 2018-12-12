179f6448bf1a8b613f3e33a0ab928307904f3651
Jelle Stege <jstege1206@gmail.com>
2018-12-12 23:24:42 +0100

    Updated 2018#12

--- aoc-2018/src/main/kotlin/nl/jstege/adventofcode/aoc2018/days/Day12.kt
+++ aoc-2018/src/main/kotlin/nl/jstege/adventofcode/aoc2018/days/Day12.kt
@@ -10,19 +10,17 @@ class Day12 : Day(title = "Subterranean Sustainability") {
         private const val SECOND_GENERATIONS = 50_000_000_000L
     }
 
-    override fun first(input: Sequence<String>): Any {
-        return input.parse().let { (initial, reps) ->
+    override fun first(input: Sequence<String>): Any =
+        input.parse().let { (initial, reps) ->
             initial.solve(FIRST_GENERATIONS, reps)
         }
-    }
 
-    override fun second(input: Sequence<String>): Any {
-        return input.parse().let { (initial, reps) ->
+    override fun second(input: Sequence<String>): Any =
+        input.parse().let { (initial, reps) ->
             initial.solve(SECOND_GENERATIONS, reps)
         }
-    }
 
-    private fun String.solve(generations: Long, replacements: Map<String, String>): Long {
+    private fun String.solve(generations: Long, replacements: Set<String>): Long {
         tailrec fun String.solveRecursive(currentGen: Long, shift: Long): Long {
             val (nextState, nextShift) = this.next(shift, replacements)
             if (nextState == this || currentGen == generations) {
@@ -37,15 +35,21 @@ class Day12 : Day(title = "Subterranean Sustainability") {
         return this.solveRecursive(0, 0)
     }
 
-    private fun String.next(shift: Long, replacements: Map<String, String>): Pair<String, Long> =
+    private fun String.next(shift: Long, replacements: Set<String>): Pair<String, Long> =
         "....$this...."
             .windowed(5, 1)
-            .joinToString("") { replacements[it] ?: "." }
-            .let { state -> Pair(state.trim('.'), shift - 2 + state.indexOf('#')) }
+            .joinToString("") { if (it in replacements) "#" else "." }
+            .let { state ->
+                Pair(state.trim('.'), shift - 2 + state.indexOf('#'))
+            }
 
-    private fun Sequence<String>.parse(): Pair<String, Map<String, String>> =
+    private fun Sequence<String>.parse(): Pair<String, Set<String>> =
         Pair(
             head.removePrefix("initial state: "),
-            drop(2).map { it.split(" => ") }.associate { (f, s) -> f to s }
+            drop(2)
+                .map { it.split(" => ") }
+                .filter { (_, s) -> s != "." }
+                .map { it.first() }
+                .toSet()
         )
 }
