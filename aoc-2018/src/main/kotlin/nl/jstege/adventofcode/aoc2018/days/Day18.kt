6e8effad0db239151cb730114ad5c2538e84e250
Jelle Stege <jstege1206@gmail.com>
2018-12-18 18:44:33 +0100

    Updated 2018#18

--- aoc-2018/src/main/kotlin/nl/jstege/adventofcode/aoc2018/days/Day18.kt
+++ aoc-2018/src/main/kotlin/nl/jstege/adventofcode/aoc2018/days/Day18.kt
@@ -2,7 +2,6 @@ package nl.jstege.adventofcode.aoc2018.days
 
 import nl.jstege.adventofcode.aoccommon.days.Day
 import nl.jstege.adventofcode.aoccommon.utils.Point
-import nl.jstege.adventofcode.aoccommon.utils.extensions.transformToIndexed
 
 class Day18 : Day(title = "Settlers of The North Pole") {
     private companion object Configuration {
@@ -21,33 +20,40 @@ class Day18 : Day(title = "Settlers of The North Pole") {
         .parse()
         .step(minutes)
         .countScores()
-        .let { (it[TREE] ?: 0) * (it[LUMBERYARD] ?: 0) }
+        .let { scores -> (scores[TREE] ?: 0) * (scores[LUMBERYARD] ?: 0) }
 
     private fun Sequence<String>.parse(): Grid = this
         .toList()
-        .let { g ->
-            Grid(g.first().length, g.size, g.flatMap { it.toList() }.toCharArray())
-        }
+        .let { g -> Grid(g.first().length, g.size, g.joinToString("") { it }) }
 
-    class Grid(private val width: Int, private val height: Int, private val grid: CharArray) {
-        fun step(limit: Int, seen: Map<String, Int> = mapOf(), step: Int = 0): Grid =
-            when {
-                step == limit -> this
-                grid.joinToString("") in seen ->
-                    (0 until (limit - step) % (step - seen[grid.joinToString("")]!!))
-                        .fold(this) { g, _ -> g.nextGrid() }
-                else -> nextGrid()
-                    .step(limit, seen + (grid.joinToString("") to step), step + 1)
-            }
+    private tailrec fun Grid.step(
+        limit: Int,
+        seen: Map<String, Int> = mapOf(),
+        step: Int = 0
+    ): Grid = when {
+        //Limit reached, return the current grid
+        step == limit -> this
+        //We've seen this grid before, this means that for every "step - seen[grid]" minutes 
+        //we will iterate over the same grids. Figure out how many steps are left to get to
+        //the limit and skip all iterations which are redundant.
+        grid in seen -> (0 until (limit - step) % (step - seen[grid]!!))
+            .fold(this) { g, _ -> g.nextGrid() }
+        //Calculate next grid, update "seen" with the current grid
+        else -> nextGrid().step(limit, seen + (grid to step), step + 1)
+    }
 
-        fun countScores(): Map<Char, Int> = grid.toList().groupingBy { it }.eachCount()
+    class Grid(private val width: Int, private val height: Int, val grid: String) {
+        fun countScores(): Map<Char, Int> = grid.groupingBy { it }.eachCount()
 
         private fun toIndex(x: Int, y: Int) = y * width + x
         private fun fromIndex(index: Int) = Point.of(index % width, index / width)
 
-        private fun nextGrid(): Grid = Grid(width, height, grid
-            .transformToIndexed(CharArray(width * height)) { index, newGrid, c ->
-                newGrid[index] = c.nextState(index.neighborValues())
+        fun nextGrid(): Grid = Grid(
+            width,
+            height,
+            //Iterate over the current grid and for every character calculate it's new value.
+            grid.withIndex().joinToString("") { (index, c) ->
+                c.nextState(index.neighborValues()).toString()
             }
         )
 
@@ -60,12 +66,11 @@ class Day18 : Day(title = "Settlers of The North Pole") {
                 .eachCount()
                 .withDefault { 0 }
 
-        private fun Char.nextState(neighbours: Map<Char, Int>): Char = when (this) {
-            OPEN_GROUND -> if (neighbours.getValue(TREE) >= 3) TREE else OPEN_GROUND
-            TREE -> if (neighbours.getValue(LUMBERYARD) >= 3) LUMBERYARD else TREE
+        private fun Char.nextState(ns: Map<Char, Int>): Char = when (this) {
+            OPEN_GROUND -> if (ns.getValue(TREE) >= 3) TREE else OPEN_GROUND
+            TREE -> if (ns.getValue(LUMBERYARD) >= 3) LUMBERYARD else TREE
             LUMBERYARD ->
-                if (neighbours.getValue(TREE) >= 1 && neighbours.getValue(LUMBERYARD) >= 1)
-                    LUMBERYARD
+                if (ns.getValue(TREE) >= 1 && ns.getValue(LUMBERYARD) >= 1) LUMBERYARD
                 else OPEN_GROUND
             else -> throw IllegalStateException()
         }
